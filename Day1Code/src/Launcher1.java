f29be3bb2535b00a381470c8167980b773889d11
RubyXing <593680319@qq.com>
2020-01-07 10:20:57 +0800

    commit from class PC

--- Day1Code/src/Launcher1.java
+++ Day1Code/src/Launcher1.java
@@ -2,12 +2,35 @@ import java.io.*;
 
 public class Launcher1 {
     public static void main(String[] args) {
+        Writer w = null;
+        BufferedWriter bf = null;
         try {
-            Writer w=new FileWriter(new File("d:\\ccc"));
-            BufferedWriter bf=new BufferedWriter(w);
-            String a = "abc";
+            w = new FileWriter(new File("d:\\ccc\\testmy.txt"), true);
+            bf = new BufferedWriter(w);
+            String str = "abc test";
+            bf.write(str);
+            bf.newLine();
+            bf.write(str);
+            bf.write(str);
+            bf.write(str);
+            bf.flush();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (bf != null) {
+                    bf.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (w != null) {
+                try {
+                    w.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
