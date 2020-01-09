83793e1df09c1cac99fcbaff37d55756012997ae
RubyXing <593680319@qq.com>
2020-01-09 19:27:57 +0800

    commit from class PC

--- Day4Code/src/com/HomeWork4.java
+++ Day4Code/src/com/HomeWork4.java
@@ -8,19 +8,20 @@ import java.util.*;
 
 public class HomeWork4 {
     public void start() {
-        worker2();
+        worker4();
     }
 
     //从键盘获取一行输入字符串，要求去除重复字符
     private void worker1() {
         Set<Character> set = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
         Scanner sc = new Scanner(System.in);
         char[] ab = sc.nextLine().toCharArray();
         for (char c : ab) {
-            set.add(c);
+            if (set.add(c)) {
+                sb.append(c);
+            }
         }
-        StringBuilder sb = new StringBuilder();
-        set.forEach(sb::append);
         System.out.println(sb);
     }
 
@@ -44,19 +45,45 @@ public class HomeWork4 {
         li.add("xyz");
         li.add("123");
         li.add("xyz");
-
+        Set<String> set = new HashSet<>(li);
+        li = new LinkedList<>(set);
+        li.forEach(System.out::println);
 
     }
 
-    //已知有十六支男子足球队参加2008 北京奥运会。把这16 支球队随机分为4 个组。采用List集合和随机数2008 北京奥运会男足参赛国家：
-//科特迪瓦，阿根廷，澳大利亚，塞尔维亚，荷兰，尼日利亚、日本，美国，中国，新西 兰，巴西，比利时，韩国，喀麦隆，洪都拉斯，意大利
+    //已知有十六支男子足球队参加2008 北京奥运会。把这16 支球队随机分为4 个组。采用List集合和随机数2008北京奥运会男足参赛国家：
+//科特迪瓦，阿根廷，澳大利亚，塞尔维亚，荷兰，尼日利亚，日本，美国，中国，新西兰，巴西，比利时，韩国，喀麦隆，洪都拉斯，意大利
     private void worker4() {
+        String[] str = "科特迪瓦，阿根廷，澳大利亚，塞尔维亚，荷兰，尼日利亚，日本，美国，中国，新西兰，巴西，比利时，韩国，喀麦隆，洪都拉斯，意大利".split("，");
+//        List<String> li=new ArrayList<>(Arrays.asList(str));
+        Set<String> set = new HashSet<>();
+        int i = 0;
+        while (set.size() < 16) {
+            int temp = (int) (Math.random() * 16);
+            if (set.add(str[temp])) {
+                System.out.print(str[temp] + "  ");
+                i++;
+                if (i == 4) {
+                    System.out.println();
+                    i = 0;
+                }
+            }
+        }
+
 
     }
 
     //5; 当前班上有同学进行报道，当学号与姓名相同时就被认为是同一个学生，统计当前班上同学姓名和人数。随意创建5个学生即可。
     private void worker5() {
-
+        Set<Students> stud = new HashSet<>();
+        stud.add(new Students("aaa", 12));
+        stud.add(new Students("bbb", 13));
+        stud.add(new Students("ccc", 14));
+        stud.add(new Students("ddd", 15));
+        stud.add(new Students("eee", 16));
+        stud.add(new Students("aaa", 12));
+        stud.add(new Students("ccc", 14));
+        stud.forEach(System.out::println);
     }
 
 }
